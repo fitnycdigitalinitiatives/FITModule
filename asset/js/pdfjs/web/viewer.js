cb396c4c2cc0bb85d94171fdd2a277cb276fdee3
Joseph Anderson <joseph_anderson@fitnyc.edu>
2021-04-27 14:40:48 -0400

    updated pdf viewer endpoint

--- asset/js/pdfjs/web/viewer.js
+++ asset/js/pdfjs/web/viewer.js
@@ -1710,7 +1710,7 @@ exports.PDFViewerApplication = PDFViewerApplication;
 let validateFileURL;
 {
   //Only will display files from our S3 endpoint
-  const ACCEPTED_FILE_ORIGINS = ["https://9f6b04d3-146c-41cd-b827-1e4281d978df.s3.amazonaws.com", "http://localhost"];
+  const ACCEPTED_FILE_ORIGINS = ["https://dip-access-bucket.s3.us-east-1.amazonaws.com"];
 
   validateFileURL = function (file) {
     if (file === undefined) {
