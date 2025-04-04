c15416ec874215a3cb921ddce7da990c3e0d5685
Joseph Anderson <joseph_anderson@fitnyc.edu>
2025-04-04 15:45:30 -0400

    Update mirador.js

--- asset/js/mirador.js
+++ asset/js/mirador.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
         window: {
             allowClose: false,
             allowFullscreen: true,
+            allowMaximize: false,
         },
         windows: [
             {
